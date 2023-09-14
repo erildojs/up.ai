5fd7301c5ebafc4e28ac3862a335b1262463c570
ErildoJS <erildofranciscojs@gmail.com>
2023-09-14 19:17:54 +0100

    feat: add textarea

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import {Github} from 'lucide-react'
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
 
@@ -22,6 +23,21 @@ export function App() {
           </Button>
         </div>
       </header>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea className="resize-none p-4 leading-relaxed" placeholder="Inclua o prompt para a AI..."/>
+            <Textarea className="resize-none p-4 leading-relaxed" placeholder="Resultado gerado pela AI..." readOnly/>
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se voce pode utilizar a variavel <code className="text-violet-400">{'{transcription}'}</code> no 
+            seu prompt para adicionar o conteudo da transcricao do video selecionado
+          </p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
     </main>
   )
 }
