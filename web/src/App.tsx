68b25a7e60080246b4c6150832d7d715d65fa888
ErildoJS <erildofranciscojs@gmail.com>
2024-01-04 23:09:15 +0100

    remove some imports thats not to be used

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
-import {Github, FileVideo, Upload, Wand2} from 'lucide-react'
+import {Github, Wand2} from 'lucide-react'
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
