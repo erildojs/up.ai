7b02b73808cd70ca90551138513969cc50d32627
ErildoJS <erildofranciscojs@gmail.com>
2024-01-07 11:26:23 +0100

    add feature of preview video selected

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -3,16 +3,38 @@ import { Separator } from '@radix-ui/react-separator'
 import {FileVideo, Upload} from 'lucide-react'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
+import { FormEvent, useMemo, useState } from 'react'
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+
+  function handleFileSelected(event: FormEvent<HTMLInputElement>) {
+    const {files} = event.currentTarget
+    if(!files) return
+    const selectedFile = files[0]
+    setVideoFile(selectedFile)
+
+  }
+  const previewURL = useMemo(() => {
+    if(!videoFile) return null
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
   return (
-    <form action="" className="space-y-6">
-            <label htmlFor="video" className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5">
-              <FileVideo className="w-4 h-4"/>
-              Selecione um Video
+    <form className="space-y-6">
+            <label htmlFor="video" className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5">
+              
+              {previewURL ? (
+                <video src={previewURL} controls={false} className='pointer-events-none absolute inset-0' />
+              ) : (
+                <>
+                <FileVideo className="w-4 h-4"/>
+                Selecione um Video
+                </>
+              )}
             </label>
 
-            <input type="file" id="video" accept="video/mp4" className="sr-only" />
+            <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected} />
 
             <Separator />
 
